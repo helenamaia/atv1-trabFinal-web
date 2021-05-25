a0d0447da308a78a27e5dfd19ebf25f556e72420
helena karol <helenadiasmaia@gmail.com>
2021-05-25 16:53:55 -0300

    create validators

--- app/Controllers/Http/RafflesController.ts
+++ app/Controllers/Http/RafflesController.ts
@@ -15,12 +15,16 @@ export default class RafllesController {
   }
 
   public async create({ view }: HttpContextContract) {
+    const raffle = new Raffle()
     const types = await Type.all()
-    return view.render('raffles/create', { types })
+    return view.render('raffles/create', { types, raffle })
   }
 
-  public async store({ request, response, auth }: HttpContextContract) {
-    const data = await request.only(['title', 'dateLikelySortition', 'dateStartSale', 'dateEndSale', 'priceTicket', 'typeId'])
+  public async store({ request, response, auth, session }: HttpContextContract) {
+    const data = await request.only(['title', 'description', 'dateLikelySortition', 'dateStartSale', 'dateEndSale', 'priceTicket', 'typeId'])
+    if (!this.validate(data, session)) {
+      return response.redirect().back()
+    }
     const user = auth.user
     const raffle = await Raffle.create({ ...data, userId: user?.id })
     let tickets = Array()
@@ -37,6 +41,25 @@ export default class RafllesController {
     return view.render('raffles/show', { raffle, types })
   }
 
+  public async edit({ params, view, auth }: HttpContextContract) {
+    const raffle = await this.getRaffle(auth, params.id, true)
+    const types = await Type.all()
+    return view.render('raffles/edit', { raffle, types })
+  }
+
+  public async update({ params, request, response, auth, session }: HttpContextContract) {
+    const raffle = await this.getRaffle(auth, params.id)
+    const data = request.only(['title', 'description', 'dateLikelySortition', 'dateStartSale', 'dateEndSale', 'priceTicket', 'typeId'])
+
+    if (!this.validate(data, session)) {
+      return response.redirect().back()
+    }
+
+    raffle.merge(data)
+    raffle.save()
+    response.redirect().toRoute('raffles.index')
+  }
+
 
   private async getRaffle(auth: AuthContract, id, preaload = false): Promise<Raffle> {
     const user = auth.user!!
@@ -47,5 +70,49 @@ export default class RafllesController {
     }
   }
 
+
+  private validate(data, session): Boolean {
+    const errors = {}
+
+    if (!data.title) {
+      this.registerError(errors, 'title', 'Campo obrigatório')
+    }
+
+    if(!data.dateLikelySortition){
+      this.registerError(errors, 'dateLikelySortition', 'Campo obrigatório')
+    }
+    if(!data.dateStartSale){
+      this.registerError(errors, 'dateStartSale', 'Campo obrigatório')
+    }
+    if(!data.dateEndSale){
+      this.registerError(errors, 'dateEndSale', 'Campo obrigatório')
+    }
+
+    if (!data.priceTicket) {
+      this.registerError(errors, 'priceTicket', 'Campo obrigatório')
+    }
+
+    if(!data.typeId){
+      this.registerError(errors, 'typeId', 'Campo obrigatório')
+    }
+    
+    
+
+    if (Object.entries(errors).length > 0) {
+      session.flash('error', 'Erro ao salvar a rifa.')
+      session.flash('errors', errors)
+      session.flashAll()
+      return false
+    }
+
+    return true
+  }
+
+  private registerError(errors, attribute, error) {
+    if (!errors[attribute]) {
+      errors[attribute] = []
+    }
+    errors[attribute].push(error)
+  }
 
 }
