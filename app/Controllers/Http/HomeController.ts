9a9633e83609cd9847d921249d755c6357377d1c
helena karol <helenadiasmaia@gmail.com>
2021-06-12 10:12:02 -0300

    home ok

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -16,13 +16,30 @@ export default class HomeController {
       const rafflesParticipate = await Database.rawQuery(
         `SELECT DISTINCT raffles.* FROM raffles, tickets WHERE tickets.user_id=${user.id} AND raffles.id=tickets.raffle_id`
       )
-      const ticketsBuy = await user.related('tickets').query().preload('raflle')
-      const total = ticketsBuy.reduce((sum, ticket) => sum + ticket.raflle.priceTicket, 0)
-      console.log(total);
+
+    
+      for (const raffle of rafflesParticipate) {
+        raffle.awards = await Database.rawQuery(`SELECT DISTINCT awards.* FROM raffles, awards WHERE awards.raffle_id=${raffle.id}`)
+        
+      
+      }
+      for (const raffle of rafflesParticipate) {
+        raffle.tickets = await Database.rawQuery(`SELECT DISTINCT tickets.* FROM raffles, tickets WHERE tickets.raffle_id=${raffle.id}`)
+        
+        console.log(raffle.tickets);
+        
+      }
+      
+
+      //const ticketsBuy = await user.related('tickets').query().preload('raflle')
+      //const total = ticketsBuy.reduce((sum, ticket) => sum + ticket.raflle.priceTicket, 0)
+      //console.log(total);
+      
+      
       
       
 
-      return view.render('home/index', {raffles, rafflesParticipate})
+      return view.render('home/index', {raffles, rafflesParticipate, user })
 
     }
   
